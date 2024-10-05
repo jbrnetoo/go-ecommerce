78888eee4218016f19b067a1f99d4c7b87e9281d
Joao Neto <joaoneto.rocha20@gmail.com>
2024-10-05 11:31:35 -0300

    feat: creation of migrations

---
+++ cmd/migrate/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	mysqlCfg "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jbrnetoo/go-ecommerce/config"
+	"github.com/jbrnetoo/go-ecommerce/db"
+)
+
+func main() {
+
+	db, err := db.NewMySQLStorage(mysqlCfg.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAdress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+
+	if cmd == "up" {
+		err := m.Up()
+		if err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Println("Executando DOWN")
+			log.Fatal(err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
 module github.com/jbrnetoo/go-ecommerce
 
-go 1.22
+go 1.22.0
+
+toolchain go1.22.5
 
 require (
 	github.com/go-playground/validator/v10 v10.22.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
-	golang.org/x/crypto v0.25.0
+	golang.org/x/crypto v0.27.0
 )
 
 require (
@@ -15,8 +17,12 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/net v0.27.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
